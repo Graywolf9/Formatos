281ea861a7eb55ca98d33afbe5cff6956dfd65dd
Alexander Alcala <alexander_belial999@hotmail.com>
2015-10-14 06:03:16 +0000

    0

--- index.php
+++ index.php
@@ -7,6 +7,11 @@
     echo "<br/>";
     system ("ls", $lista);
     echo $lsita;
+    echo "<br/>";
     system ("echo ".$encabezado. " >> ejemlo.txt", $ejemplo);
+    echo "<br/>";
+    echo $ejemplo;
+    system ("touch ejemplo.txt", $ejemplo);
+    echo "<br/>";
     echo $ejemplo;
 ?>
