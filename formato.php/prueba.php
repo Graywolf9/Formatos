478000826993ae3a55f4590d4edb81b48df6d746
Alexander Alcala <alexander_belial999@hotmail.com>
2015-10-16 04:42:10 +0000

    0

--- formato.php/prueba.php
+++ formato.php/prueba.php
@@ -1,13 +1,11 @@
 <?php
-    $file = $_POST["tipo"]."_".$_POST["nombre"];
+    $file = $_POST["tipo"]."_".$_POST["oficio"]."_".date_timestamp_get(date_create());;
     $ruta = "../"; // /Formatos/
     $formato = "prueba";
     $argumentos[0] = $encabezado = "\"__Encabezado__\"=\"".$_POST["encabezado"]."\"";
     $argumentos[1] = $pie = "\"__Pie__\"=\"".$_POST["pie"]."\"";
     
     system ("cp ".$ruta."formato/".$formato.".svg ".$ruta."oficio/$file.svg", $result);
-    echo "cp ".$ruta."formato/".$formato.".svg ".$ruta."oficio/$file.svg";
-    echo $result;
     for ($x=0;$x<count($argumentos);$x++){
         system ("echo $(sed s=$argumentos[$x]= ".$ruta."oficio/$file.svg) > ".$ruta."oficio/$file.svg", $result);
     }
